7ac5e9548facfd2593ac7d3bf252af405820517a
Adam Mawlawi <adam@mradam.us>
2021-01-22 13:01:31 -0600

    create FaqSection section and add it to aboutUs page

---
+++ src/components/FaqSection.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const FaqSection = () => {
+  return (
+    <div>
+      <div className='faq'>
+        <h2>
+          Any questions <span>FAQ</span>
+        </h2>
+        <div className='question'>
+          <h4>How Do I Start?</h4>
+          <div>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+        </div>
+        <div className='question'>
+          <h4>How Do I Start?</h4>
+          <div>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+        </div>
+        <div className='question'>
+          <h4>How Do I Start?</h4>
+          <div>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+        </div>
+        <div className='question'>
+          <h4>How Do I Start?</h4>
+          <div>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
