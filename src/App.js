5b4f3f8f911078ddc5739d2da8312ce74c370f61
Adam Mawlawi <adam@mradam.us>
2021-01-22 21:21:15 -0600

    add react Router Switch and Link to my pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import OutWork from './pages/OutWork';
 import GlobalStyle from './components/GlobalStyle';
 import Nav from './components/Nav';
+//Router
+import { Switch, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className='App'>
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path='/' exact>
+          <AboutUs />
+        </Route>
+        <Route path='/work'>
+          <OutWork />
+        </Route>
+        <Route path='/contact'>
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/OutWork.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OutWork = () => {
+  return (
+    <div>
+      <h1>Our work</h1>
+    </div>
+  );
+};
+
+export default OutWork;
