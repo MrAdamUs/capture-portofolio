a72e5d87d65be3a54554b31b335f71adf415ab5a
Adam Mawlawi <adam@mradam.us>
2021-01-22 20:27:43 -0600

    styleing FAQ

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,43 +1,78 @@
 import React from 'react';
+import styled from 'styled-components';
+import { About } from '../styles';
 
 const FaqSection = () => {
   return (
-    <div>
+    <Faq>
       <div className='faq'>
         <h2>
           Any questions <span>FAQ</span>
         </h2>
         <div className='question'>
           <h4>How Do I Start?</h4>
-          <div>
+          <div className='answer'>
             <p>Lorem ipsum dolor sit amet.</p>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
           </div>
+          <div className='faq-line'></div>
         </div>
         <div className='question'>
           <h4>How Do I Start?</h4>
-          <div>
+          <div className='answer'>
             <p>Lorem ipsum dolor sit amet.</p>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
           </div>
+          <div className='faq-line'></div>
         </div>
         <div className='question'>
           <h4>How Do I Start?</h4>
-          <div>
+          <div className='answer'>
             <p>Lorem ipsum dolor sit amet.</p>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
           </div>
+          <div className='faq-line'></div>
         </div>
         <div className='question'>
           <h4>How Do I Start?</h4>
-          <div>
+          <div className='answer'>
             <p>Lorem ipsum dolor sit amet.</p>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
           </div>
+          <div className='faq-line'></div>
         </div>
       </div>
-    </div>
+    </Faq>
   );
 };
+
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+
+  .faq-line {
+    background: #ccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .amswer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 
 export default FaqSection;
