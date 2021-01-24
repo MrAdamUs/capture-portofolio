573ef23fa04f42d804d19c98bb1ba0dcf18406e1
Adam <adam@mradam.us>
2021-01-24 16:02:34 -0600

    working on details page

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
+import { movieState } from '../movieState';
 
 const MovieDetail = () => {
+  const history = useHistory();
+  const url = history.location.pathname;
+  const [movies, setMovies] = useState(null);
+
+  //useEffect
+  useEffect(() => {
+    const currentMovie = movies.filter((stateMovie) => stateMovie.url === url);
+    setMovies(currentMovie);
+  }, [movies, url]);
   return (
     <div>
       <h1>MovieDetail</h1>
